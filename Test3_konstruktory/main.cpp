37d443ff15e23f3df909c607c01c92b4e859379a
BaskaKlim <baska.klimekova@gmail.com>
2020-05-21 00:16:01 +0200

    constructor with parameter

--- Test3_konstruktory/main.cpp
+++ Test3_konstruktory/main.cpp
@@ -8,9 +8,18 @@ public:
     Square() {
         a = 0.0;
     }
+
+    Square(double a) {
+        this->a = a;
+    }
 };
 
 int main() {
     Square c1;        // a bude mit hodnotu 0.0
+    Square c2(3.0);    // n bude mit hodnotu 3.0
+    printf("Ctverec ma polomer %lf\n", c1.a);
+    printf("Ctverec ma polomer %lf\n", c2.a);
+
+    return 1;
 
 }
