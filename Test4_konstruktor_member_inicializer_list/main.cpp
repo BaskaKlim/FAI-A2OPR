7d42f6d3198655a1021a8b3f70f4d0aa3ae9a9ca
BaskaKlim <baska.klimekova@gmail.com>
2020-05-21 01:56:05 +0200

    using class withoud defalut constructor, another class with this extends class using constructor list

--- Test4_konstruktor_member_inicializer_list/main.cpp
+++ Test4_konstruktor_member_inicializer_list/main.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
 
-class Obdelnik {
-public:
-    double m;
-    double n;
-
-//inicializacia variables
-    Obdelnik() : m(0.0), n(0.0) {
-
-    }
-};
-
 // constructor member inicializer list , Tento zápis musíme ho použít pokud třída nemá výchozí konstruktor bez parametrů.
 class Souradnice {
 public:
@@ -21,9 +10,28 @@ public:
 
     }
 };
+class Obdelnik {
+public:
+
+    const int id;
+    double m;
+    double n;
+    Souradnice stred;
+
+//inicializacia variables
+    Obdelnik(int id, double m, double n, double x, double y) : id(id), m(m), n(n), stred(x, y)
+    {
+
+    }
+};
+
+
 
 
 int main() {
-    Obdelnik o1;
+    Obdelnik o1(1, 2.0, 3.0, 100, 200);
+    printf("id %d: %lf x %lf, x: %lf, y: %lf", o1.id, o1.m, o1.n, o1.stred.x, o1.stred.y);
+
+    return 1;
 
 }
