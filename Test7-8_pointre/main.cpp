cfccebd6d1aab3609f46980d4a4afdc31c116808
BaskaKlim <baska.klimekova@gmail.com>
2020-05-21 12:33:42 +0200

    adresny operator a operator indirekcie

---
+++ Test7-8_pointre/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+//TODO: Nejprve si definujeme třídu Pole která si v konstruktoru alokuje paměť.
+
+int main() {
+    //adresny operator alebo aj tzv operator referencie
+    int x = 2;
+    int* p = &x;
+
+    //Pokud pracujeme přímo s ukazatelem, tak pracujeme s jeho hodnotou což je adresa.
+    int z = 2;
+    int* p1 = &z;
+    int* p2 = p1;
+    //Pokud chceme pracovat s hodnotou uloženou na adrese na kterou ukazatel ukazuje, tak musíme použít operátor indirekce = operátor dereference
+    *p2 = 0;
+    *p2 = *p2 + 1;
+    *p2 += 1;
+    ++*p2;
+    (*p2)++;
+
+    printf("%p,%p,%p ", &p,&p1,&p2);
+    printf("\n%d,%d,%d ", p,p1,p2);
+}
