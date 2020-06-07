36ef97df5a18493d888399029acbe8e57c3a7c52
BaskaKlim <baska.klimekova@gmail.com>
2020-06-07 23:07:41 +0200

    LINE -> declaration of lines via position points

--- DrawSquares/main.cpp
+++ DrawSquares/main.cpp
@@ -6,13 +6,22 @@ struct Position {
     double x;
     double y;
 
-    Position(double x, double y) : x(x), y(y)
-    {
+    Position(double x, double y) : x(x), y(y) {
     }
 };
 
-int main()
-{
-    Position p1(2,3);
-    Position p2(5,7);
+int main() {
+    Position p1(2, 3);
+    Position p2(5, 7);
+
+//line and its properties
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+
+//lenght of line  = max rozdiel bodov (ich absolutnych hodnot)
+    double d = std::fmaxf(std::abs(dx), std::abs(dx));
+
+    dx /= d;
+    dy /= d;
+
 }
