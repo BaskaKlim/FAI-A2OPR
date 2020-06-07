2fcbe259fa23cf0de9c18c2b4bc8f2ee20529858
BaskaKlim <baska.klimekova@gmail.com>
2020-06-07 23:26:58 +0200

    adding logical steps to draw line

--- DrawSquares/main.cpp
+++ DrawSquares/main.cpp
@@ -24,18 +24,42 @@ int main() {
     dx /= d;
     dy /= d;
 
-// board for painting 
+// board for painting
     constexpr int numberOfRow = 30;
     constexpr int numberOfColumn = 30;
 
     char board[numberOfRow][numberOfColumn];
 
-    for(size_t i =0; i< numberOfRow; i++)
-    {
-        for (size_t j =0; j< numberOfColumn; j++)
-        {
-            board[i][j]= '.';
+    for (size_t i = 0; i < numberOfRow; i++) {
+        for (size_t j = 0; j < numberOfColumn; j++) {
+            board[i][j] = '.';
         }
     }
 
+//nastavenie vychodzych bodov
+    double x = p1.x;
+    double y = p1.y;
+
+    for (int i = 0; i <= d; i++) {
+        int ix = (int) std::round(x);
+        int iy = (int) std::round(y);
+
+        //
+        board[iy][ix] = 'x';
+
+        x+= dx;
+        y+= dy;
+    }
+
+    for(size_t i = 0; i <numberOfRow; i++)
+    {
+        for(size_t j = 0; i <numberOfColumn; j++)
+        {
+            char ch = board[numberOfRow -1-i][j];
+            putchar(ch);
+        }
+        putchar('\n');
+
+    }
+return  0;
 }
